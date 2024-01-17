9e6a6fc5f4088916e0aeed4e07b6583a4bd54959
Asmaa231 <asmaasalem231@gmail.com>
2024-01-17 00:56:46 -0800

    a

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ mod = number % 10 if number > 10 else number % -10
 print("Last digit of {:d} is {:d} and is "
       .format(number, mod), end="")
 if mod > 5:
-    print("graeter than 5")
+    print("greater than 5")
 if mod == 0:
     print("0")
 else:
