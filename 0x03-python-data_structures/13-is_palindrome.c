fdbce876618a0c7cad84de2a900c11aa8a46a505
Asmaa231 <asmaasalem231@gmail.com>
2024-01-22 11:40:41 -0800

    a

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,11 +5,11 @@
  *Return: 0 if not palind, 1 if palind
  */
 
-int is_palindrome(listint_t **head);
+int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
 		return (1);
-	return (aux_palind(head, *head))
+	return (aux_palind(head, *head));
 }
 
 /**
@@ -21,7 +21,7 @@ int is_palindrome(listint_t **head);
 int aux_palind(listint_t **head, listint_t *end)
 {
 	if (end == NULL)
-		return = (1);
+		return (1);
 	if (aux_palind(head, end->next) && (*head)->n == end->n)
 	{
 		*head = (*head)->next;
