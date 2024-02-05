f91310badaef0ccea81459bba14681e3c9547a09
Asmaa231 <asmaasalem231@gmail.com>
2024-02-05 13:29:01 -0800

    a

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+import sys
+
+def safe_print_integer_err(value):
+    is_int = True
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        print("Exception:", e, file-sys.stderr)
+        is_int = False
+    return is_int
