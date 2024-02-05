828051d01d800a1a0af4ff31027b5a8d2ae2536f
Asmaa231 <asmaasalem231@gmail.com>
2024-02-05 09:57:34 -0800

    a

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!user/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    nwlist = []
+
+    for i in range(list_length):
+        try:
+            nwlist.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            nwlist.append(0)
+            print('division by 0')
+            continue
+        except IndexError:
+            nwlist.append(0)
+            print('out of range')
+            continue
+        except TypeError:
+            nwlist.append(0)
+            print('wrong type')
+            continue
+        finally:
+            pass
+
+    return nwlist
