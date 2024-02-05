435bf4c5f1de1f575e21e75903214a37ed15a10a
Asmaa231 <asmaasalem231@gmail.com>
2024-02-05 10:17:42 -0800

    a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             i += 1
    except IndexError:
        None
-       print()
-       return i
+   print()
+   return i
