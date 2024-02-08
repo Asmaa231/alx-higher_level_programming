716582a5af9c2b0eaa6a7a5f2231673b8e90ffcc
Asmaa231 <asmaasalem231@gmail.com>
2024-02-08 09:37:18 -0800

    a

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
@@ -6,4 +7,3 @@ class Square:
          if size < 0:
              raise ValueError('size must be >= 0')
          self.__size = size
-
