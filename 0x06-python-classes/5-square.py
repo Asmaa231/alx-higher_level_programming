548b76ec397cc0d29dd2cd2c797caae05e5ab507
Asmaa231 <asmaasalem231@gmail.com>
2024-02-09 07:48:36 -0800

    a

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""Square module."""
+
 
 class Square:
+    """define a square."""
     def __init__(self, size=0):
+        """constructor
 
+        Args:
+            size: square side length
+        """
         self.size = size
 
     @property
     def size(self):
+        """Property for square side length
+
+        Raises:
+           TypeError: if size is not int
+           ValueError: if zise is negative
+        """
         return self.__size
 
     @size.setter
@@ -18,9 +31,14 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Area of square
+
+        Return: Square area
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """Print the square with #"""
         for i in range(self.size):
             for j in range(self.size):
                 print("#", end="\n" if j is self.size - 1 and i != j else "")
