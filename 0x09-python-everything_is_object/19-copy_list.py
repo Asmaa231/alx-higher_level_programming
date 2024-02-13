180e2e7db4773dfd362d64f10746f79ae7b563f8
Asmaa231 <asmaasalem231@gmail.com>
2024-02-13 11:56:07 -0800

    a

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def copy_list(mylist):
-    """Returns a string “BestSchool” n times
-        the number of the iteration
-        Args: mylist
-    """
-     return (mylist.copy())
+    return (mylist.copy())
