369712214afea02e7bb2d43606202f77221ee014
Asmaa231 <asmaasalem231@gmail.com>
2024-02-19 11:51:13 -0800

    a

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A basegeometry class"""
+    def area(self):
+        """methode to compute area"""
+        raise Exception('area() is not implemented')
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A basegeometry class"""
+    def area(self):
+        """methode to compute area"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """method for validating value"""
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{}  must be greater than 0'.format(name))
