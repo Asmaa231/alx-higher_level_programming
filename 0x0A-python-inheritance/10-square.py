f47de91c9f1f017667239f0d51402f7ebf5016c1
Asmaa231 <asmaasalem231@gmail.com>
2024-02-19 13:43:38 -0800

    a

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,7 +9,7 @@ class Square(BaseGeometry):
         """constructor"""
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self, size)
+        super().__init__(size, size)
 
     def area(self):
         """methode return square area"""
