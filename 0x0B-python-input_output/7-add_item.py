d69a4e672ba9b201fba329f99697ff4e8c4b5770
Asmaa231 <asmaasalem231@gmail.com>
2024-02-20 12:51:42 -0800

    a

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,8 @@
 """Module task 7"""
 
 
-import json
-
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
+ave_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
