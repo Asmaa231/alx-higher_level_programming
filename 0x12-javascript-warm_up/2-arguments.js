0f56882e21af7ede293c4fab7b7607a574be1221
Asmaa231 <asmaasalem231@gmail.com>
2024-04-24 14:57:21 -0700

    a

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+const args = process.argv;
 
-if (process.argv.length === 2) {
-	console.log('No argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
+if (args.length <= 2) {
+        console.log('No argument');
+} else if (args.length === 3) {
+        console.log('Argument found');
 } else {
-	console.log('Arguments found');	
+        console.log('Arguments found');
 }
