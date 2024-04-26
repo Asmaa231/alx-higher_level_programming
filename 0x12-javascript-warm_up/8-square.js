ea8f1f399f0a4abb7634c50dd65be6a014cffa08
Asmaa231 <asmaasalem231@gmail.com>
2024-04-26 06:16:58 -0700

    a

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,10 +6,10 @@ if (size === undefined || isNaN(parseInt(size))) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(size); i++) {
-	let row = '';
-	for (let j = 0; j < parseInt(size); j++) {
-		row += X;
-	}
-	console.log(row);
+    let row = '';
+    for (let j = 0; j < parseInt(size); j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
 }
