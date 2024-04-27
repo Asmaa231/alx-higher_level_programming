5709466302502ebdb49d493e8b54843171f3b1c7
Asmaa231 <asmaasalem231@gmail.com>
2024-04-26 22:34:50 -0700

    a

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,14 +2,14 @@
 
 let args = parseInt(process.argv[2]);
 
-if (isNaN(args)){
+if (isNaN(args)) {
   args = 1;
 }
 
 function factorial (n) {
-  if (n == 1) {
+  if (n === 1) {
     return 1;
   }
-  return n * factorial(n -1);
+  return n * factorial(n - 1);
 }
 console.log(factorial(args));
